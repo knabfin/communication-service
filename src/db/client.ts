d0f1784ee0120341426192fcd813ba99ba7c191d
Swetashree05 <swetashree.k@knabfiance.com>
2025-11-25 18:28:56 +0530

    wip

--- src/db/client.ts
+++ src/db/client.ts
@@ -3,9 +3,17 @@ import { Pool } from 'pg';
 import * as schema from './schema';
 import 'dotenv/config';
 // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
-const pool = new Pool({
+console.log('Db configs:::::::', {
   host: process.env.PG_HOST ?? 'localhost',
   port: Number(process.env.PG_PORT ?? 5433),
+  user: process.env.PG_USER ?? 'postgres',
+  password: process.env.PG_PASSWORD ?? 'postgres',
+  database: process.env.PG_DB ?? 'communicationdb',
+});
+
+const pool = new Pool({
+  host: process.env.PG_HOST ?? 'localhost',
+  port: Number(process.env.PG_PORT ?? 5432),
   user: process.env.PG_USER ?? 'postgres',
   password: process.env.PG_PASSWORD ?? 'postgres',
   database: process.env.PG_DB ?? 'communicationdb',
